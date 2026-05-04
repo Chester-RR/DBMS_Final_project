c81e2f32617f08e1eb1baa545916dee7500b11ac
Chester-RR <chesterhsu27@gmail.com>
2026-05-04 12:04:28 +0800

    引入spring boot架構

---
+++ nonsense_text_generation_model/src/test/java/com/example/nonsense_text_generation_model/NonsenseTextGenerationModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nonsense_text_generation_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonsenseTextGenerationModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
